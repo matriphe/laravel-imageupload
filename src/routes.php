abb0b8698a1b280ef17d41b4e9b6b44ad9fc9a2b
Muhammad Zamroni <halo@matriphe.com>
2015-03-28 21:05:16 +0700

    Add post handling.

--- src/routes.php
+++ src/routes.php
@@ -2,5 +2,9 @@
 
 Route::any('matriphe/imageupload', function() 
 {
+    if (Request::hasFile('file')) {
+        $result = Imageupload::upload(Request::file('file'));
+    }
+    
     return view('imageupload::form');
 });
