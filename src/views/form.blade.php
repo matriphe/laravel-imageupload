0994a0ec39fb928967ed151ccc1507fdd6036405
Muhammad Zamroni <halo@matriphe.com>
2015-03-28 22:39:45 +0700

    Change form.blade.php

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Imageupload</title>
-  </head>
-  <body>
-    <form action="{{ URL::current() }}" method="post" enctype="multipart/form-data">
-      <div>
-      <input type="file" name="file" />
-      </div>
-      <div>
-      <button type="submit">Upload!</button>
-      </div>
-    </form>
-  </body>
+    <head>
+        <title>Imageupload</title>
+    </head>
+    <body>
+        <form action="{{ URL::current() }}" method="post" enctype="multipart/form-data">
+            <input type="hidden" name="_token" value="{{ Session::token() }}">
+            <pre>{{ (!empty($result) ? print_r($result, 1) : '') }}</pre>
+            <div>
+                <input type="file" name="file">
+            </div>
+            <div>
+                <button type="submit">Upload!</button>
+            </div>
+        </form>
+    </body>
 </html>
