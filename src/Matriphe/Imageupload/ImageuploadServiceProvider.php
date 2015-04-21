abeb32b4623955964a4c0a24259658d1693ad58a
Muhammad Zamroni <halo@matriphe.com>
2015-04-22 04:56:02 +0700

    Ups! Wrong serviceprovider.

--- src/Matriphe/Imageupload/ImageuploadServiceProvider.php
+++ src/Matriphe/Imageupload/ImageuploadServiceProvider.php
@@ -1,22 +1,45 @@
-<?php namespace Matriphe\Format;
+<?php namespace Matriphe\Imageupload;
 
 use Illuminate\Support\ServiceProvider;
 
-class FormatServiceProvider extends ServiceProvider {
+class ImageuploadServiceProvider extends ServiceProvider {
 
-  protected $defer = false;
+	protected $defer = false;
 
-  public function boot()
-  {
+	public function boot()
+	{
+		//include __DIR__.'/../../routes.php';
+		
+		$this->loadViewsFrom(__DIR__.'/../../views', 'imageupload');
+		$this->mergeConfigFrom( __DIR__.'/../../config/config.php', 'imageupload');
 
-  }
+        $this->publishes([
+            //__DIR__.'/../../views' => base_path('resources/views/vendor/imageupload'),
+            __DIR__.'/../../config/config.php' => config_path('imageupload.php'),
+        ]);
+	}
 
-  public function register()
-  {
-    $this->app['format'] = $this->app->share(function($app)
-    {
-      return new Format();
-    });
-  }
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['imageupload'] = $this->app->share(function($app)
+        {
+            return new Imageupload();
+        });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
 
 }
